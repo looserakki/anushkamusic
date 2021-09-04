c8b6d4c31f26419e2698d97af1ea2ae0ba263568
shubham-king <84066395+shubham-king@users.noreply.github.com>
2021-09-04 11:48:35 +0530

    Update callsmusic.py

--- plugins/king/callsmusic.py
+++ plugins/king/callsmusic.py
@@ -18,8 +18,8 @@
 from pyrogram import Client
 from pytgcalls import PyTgCalls
 
-from DaisyXMusic.config import API_HASH, API_ID, SESSION_NAME
-from DaisyXMusic.services.queues import queues
+from config import API_HASH, API_ID, SESSION_NAME
+from plugins.cassate.queues import queues
 
 client = Client(SESSION_NAME, API_ID, API_HASH)
 pytgcalls = PyTgCalls(client)
