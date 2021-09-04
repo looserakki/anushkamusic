e65af1fc5fccf4013ee0192e0774627f358b772d
shubham-king <84066395+shubham-king@users.noreply.github.com>
2021-09-04 12:02:25 +0530

    Add files via upload

---
+++ plugins/ping.py
@@ -0,0 +1,22 @@
+import os
+import asyncio
+
+from pyrogram import Client, filters
+
+
+
+@Client.on_message(filters.command("ping"))
+async def ping(client, m: Message):
+   if m.chat.type == 'private':
+       await m.reply(f"**Hello You don't worry TG VC VIDEO STREAMER is Online\nPowered by @RobotTech_official ",   
+       
+                               )
+                           
+@Client.on_message(filters.command("ping"))
+async def pibg(client, m: Message):
+   if m.chat.type == 'group':
+       await m.reply(f"** Hello You don't worry TG VC VIDEO STREAMER is Online\nPowered by @RobotTech_official ",  
+       
+                                )
+                            
+                            
