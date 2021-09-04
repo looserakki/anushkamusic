bf096a68b70acd482feb656299faab730257a1fd
shubham-king <84066395+shubham-king@users.noreply.github.com>
2021-09-04 12:27:24 +0530

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -13,7 +13,7 @@ async def ping(client, m: Message):
                                )
                            
 @Client.on_message(filters.command("ping"))
-async def pibg(client, m: Message):
+async def ping(client, m: Message):
    if m.chat.type == 'group':
        await m.reply(f"** Hello You don't worry TG VC VIDEO STREAMER is Online\nPowered by @RobotTech_official ",  
        
