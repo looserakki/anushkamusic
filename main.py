81ac52c67aabc80de703067019a8a1f47bf9cf38
PATRICIAGANG <85884951+PATRICIAGANG@users.noreply.github.com>
2021-09-09 00:24:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
+import asyncio
 from pyrogram import Client, idle
 from config import API_ID, API_HASH, BOT_TOKEN
-from plugins.vc import app
+from plugin.videoplayer import app
+from plugin.videoplayer import call_py
 bot = Client(
     ":memory:",
     API_ID,
     API_HASH,
     bot_token=BOT_TOKEN,
-    plugins=dict(root="plugins"),
+    plugins=dict(root="player"),
 )
+
 
 bot.start()
 app.start()
+call_py.start()
 idle()
